3eb057866712807c4792a4179fd6c0726a648d35
Tobias Markus <tobbi.bugs@googlemail.com>
2013-01-24 15:12:22 +0100

    Saving joined users to disk so people who once joined will not be greeted again.

--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 exports.joined = []
 exports.moderators = {}
 
@@ -18,6 +20,41 @@ exports.tell_moderators = function(message,client){
   }
 }
 
+exports.save_seen = function(json){
+  var joined_users_object = {
+    joined: json || exports.joined
+  };
+  fs.writeFile("joined.json", JSON.stringify(joined_users_object), function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("Joined users updated successfully!");
+    }
+  });
+}
+
+exports.load_seen = function(){
+  fs.readFile('joined.json', function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      var json_object = JSON.parse(data);
+      exports.joined = json_object.joined;
+      console.log("Joined users loaded successfully!");
+    }
+  });
+}
+
+exports.delete_seen = function(){
+  fs.unlink('joined.json', function (err) {
+    if (err) {
+      console.log(err);
+    }
+    console.log('Joined users deleted successfully!');
+  });
+}
+
 setInterval(function(){
   exports.joined=[]
+  exports.delete_seen();
 }, 604800000)//one week
